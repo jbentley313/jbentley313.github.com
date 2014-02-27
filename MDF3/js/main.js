edda01ad18d8138abcc50c42df127a3d47b1e151
Jason Bentley <jbxfade@fullsail.edu>
2014-02-26 22:27:15 -0800

    js fix revert

--- MDF3/js/main.js
+++ MDF3/js/main.js
@@ -2,32 +2,28 @@
 $(document).bind("pageinit", function(){
 
 	var save = $("#submit");
-	
-	
+
+
 	//save on click handler
 	save.on("click", function(){
-		
+
 		var lastName = $("#studentLastName").val();
 		var firstName = $("#studentFirstName").val();
 		var score = $("#score").val();
 
-		
-			//call method on device
-			Native.collectScore(lastName,firstName, score);
 
-			$["home"].reset(); 
-		}
-		
+		alert(score + lastName + firstName);
 
-		
+		//call method on device
+		Native.collectScore(lastName,firstName, score);
+		$["home"].reset(); 
 
+	});
 
-
-		var scoresList = $("#scores");
-		scoresList.on("click", function () {
+var scoresList = $("#scores");
+scoresList.on("click", function () {
 	// alert("display scores clicked!");
 	Native.displayScores();
 })
 
-	});
 });
